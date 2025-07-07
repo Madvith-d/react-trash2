4ccfacc9e959dfb82e984bbb109895281097607d
Madvith-d <madroux930@gmail.com>
2025-07-07 10:49:50 +0530

    ....

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import { useState } from 'react'
 
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import About from "./pages/About2"
 
 function App() {
   return(
-    <div>
+    <>
+      <div>
       hello
     </div>
+    <Routes>
+      <Route path="/about" element={<About />}  /> 
+    </Routes>
+    
+    </>
   )
 
   
